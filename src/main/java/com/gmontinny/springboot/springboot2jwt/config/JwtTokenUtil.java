e7b3403c60dcf53ff382e207514d7ed3a9274253
Giovanny Montinny de Almeida Dantas <gmontinny@hotmail.com>
2022-11-14 09:39:07 -0400

    Update JwtTokenUtil.java

--- src/main/java/com/gmontinny/springboot/springboot2jwt/config/JwtTokenUtil.java
+++ src/main/java/com/gmontinny/springboot/springboot2jwt/config/JwtTokenUtil.java
@@ -20,12 +20,12 @@ public class JwtTokenUtil {
     @Value("${jwt.secret}")
     private String secret;
 
-    //retrieve username from jwt token
+    //recuperar nome de usuário do token jwt
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
 
-    //retrieve expiration date from jwt token
+    //recuperar data de expiração do token jwt
     public Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
     }
@@ -35,28 +35,28 @@ public class JwtTokenUtil {
         return claimsResolver.apply(claims);
     }
 
-    //for retrieving any information from token we will need the secret key
+    //para recuperar qualquer informação do token, precisaremos da chave secreta
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
 
-    //check if the token has expired
+    //verifique se o token expirou
     private Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
 
-    //generate token for user
+    //gerar token para usuário
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return doGenerateToken(claims, userDetails.getUsername());
     }
 
-    //while creating the token -
-    //1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
-    //2. Sign the JWT using the HS512 algorithm and secret key.
-    //3. According to JWS Compact Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
-    //   compaction of the JWT to a URL-safe string
+    //ao criar o token -
+    //1. Defina declarações do token, como Emissor, Expiração, Assunto e o ID
+    //2. Assine o JWT usando o algoritmo HS512 e a chave secreta.
+    //3. De acordo com a serialização compacta do JWS (https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
+    // compactação do JWT para uma string segura para URL
     private String doGenerateToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY
@@ -64,7 +64,7 @@ public class JwtTokenUtil {
                 .signWith(SignatureAlgorithm.HS512, secret).compact();
     }
 
-    //validate token
+    //validar token
     public boolean isValidToken(String token, UserDetails userDetails) {
         final String username = getUsernameFromToken(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
